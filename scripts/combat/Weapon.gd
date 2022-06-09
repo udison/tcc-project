5c2bdfa549ffeb06c222de6caed6b28d575fcd1c
Roque <roque-100@hotmail.com>
2022-06-08 22:18:43 -0300

    added accuracy system

--- scripts/combat/Weapon.gd
+++ scripts/combat/Weapon.gd
@@ -11,6 +11,9 @@ enum WeaponTypes {
 @export var mag_size: int = 30
 @export var fire_rate: float = 100.0 # Bullets per minute
 @export var reload_time: float = 2.0 # In seconds
+@export var bullets_shot_once: int = 1
+@export_range(0, 90) var spread_radius: float = 15.0 # In degrees
+@export_range(1, 5) var spread_walking_multiplier: float = 1.3
 @export var bullet_scene: PackedScene
 
 var ammo: int = 0
@@ -19,7 +22,8 @@ var can_shoot: bool = true
 @onready var muzzle: Node2D = $Muzzle
 @onready var fire_timer: Timer = $FireTimer
 @onready var reload_timer: Timer = $ReloadTimer
-@onready var held_by_player: bool = get_parent().get_parent() is Player
+@onready var holder: Entity = get_parent().get_parent()
+@onready var held_by_player: bool = holder is Player
 
 enum FireModes {
 	SINGLE,
@@ -41,16 +45,25 @@ func setup_listeners():
 	input_handler.connect('reload', reload)
 
 
+func get_bullet_rotation():
+	var spread_multiplier = 1 if held_by_player and holder.motion == Vector2.ZERO else spread_walking_multiplier
+	var half_spread_degree = spread_radius * spread_multiplier / 2
+	var random_spread = deg2rad(randf_range(-half_spread_degree, half_spread_degree))
+	
+	return muzzle.global_rotation + random_spread
+
+
 func fire():
 	if !can_shoot or ammo <= 0:
 		return
 	
-	var bullet_instance = bullet_scene.instantiate()
-	bullet_instance.global_position = muzzle.global_position
-	bullet_instance.global_rotation = muzzle.global_rotation
-	bullet_instance.shot_by_player = held_by_player
-	bullet_instance.damage = damage
-	get_tree().current_scene.add_child(bullet_instance)
+	for i in bullets_shot_once:
+		var bullet_instance = bullet_scene.instantiate()
+		bullet_instance.global_position = muzzle.global_position
+		bullet_instance.global_rotation = get_bullet_rotation()
+		bullet_instance.shot_by_player = held_by_player
+		bullet_instance.damage = damage
+		get_tree().current_scene.add_child(bullet_instance)
 	ammo -= 1
 	
 	can_shoot = false
